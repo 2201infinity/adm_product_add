5522151eb1c3d1536e8c43f0ae7583ee779c4ea4
kykim00 <jeongsaeki@likelion.org>
2022-01-28 19:05:44 +0900

    feat: 상품 기한 설정 페이지 구현 refactor: disabled props로 받아오기 style: 커서 깜빡임 수정 fix: label 클릭 되도록 수정

--- src/components/ProductOption.js
+++ src/components/ProductOption.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React from 'react';
+import { ProductPeriod } from './period/Period';
 
 function ProductOption() {
-  return <div>ProductOption</div>;
+  return (
+    <div>
+      ProductOption
+      <ProductPeriod />
+    </div>
+  );
 }
 
 export default ProductOption;
