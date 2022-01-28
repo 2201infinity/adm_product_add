7d2088a8427526f65e0982fa0a9678e9ff412edc
yezyvibe <yeji0501@gmail.com>
2022-01-29 00:19:39 +0900

    feat: 검색창 클릭 시 모든 필터 출력, 태그 선택 및 해제 기능 구현

---
+++ src/components/common/FilterTag.js
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import styled from "styled-components";
+import Input from "components/common/Input";
+import Button from "components/common/Button";
+import React from "react";
+
+const tags = [
+  { id: "0", name: "tag0", checked: false },
+  { id: "1", name: "tag1", checked: false },
+  { id: "2", name: "tag2", checked: false },
+  { id: "3", name: "tag3", checked: false },
+  { id: "4", name: "tag4", checked: false },
+  { id: "5", name: "tag5", checked: false },
+  { id: "6", name: "tag6", checked: false },
+  { id: "7", name: "tag7", checked: false },
+  { id: "8", name: "tag8", checked: false },
+  { id: "9", name: "tag9", checked: false },
+  { id: "10", name: "tag10", checked: false },
+];
+
+function FilterTag() {
+  const [tagList, setTagList] = useState(tags);
+  const [focusOn, setFocusOn] = useState(false);
+
+  const onToggleChecked = (id) => {
+    setTagList((prev) =>
+      prev.map((item) =>
+        item.id === id ? { ...item, checked: !item.checked } : item
+      )
+    );
+  };
+
+  const onClick = (id) => {
+    onToggleChecked(id);
+  };
+
+  const onFocusInput = () => {
+    setFocusOn((prev) => !prev);
+  };
+  return (
+    <FilterBox>
+      <span>필터 태그</span>
+      <Input onClick={onFocusInput} placeholder="검색어를 입력하세요." />
+      {focusOn && <span>지정된 필터 태그</span>}
+      <SelectedTagList>
+        {tagList.map((item) => (
+          <React.Fragment key={`tag_list${item.id}`}>
+            {item.checked && (
+              <SelectedTag>
+                {item.name}
+                <Button size="sm" onClick={() => onClick(item.id)}>
+                  x
+                </Button>
+              </SelectedTag>
+            )}
+          </React.Fragment>
+        ))}
+      </SelectedTagList>
+
+      {focusOn && (
+        <TagList>
+          {tags.map((item) => (
+            <React.Fragment key={`tag_list${item.id}`}>
+              {!item.checked && (
+                <Tag onClick={() => onClick(item.id)}>{item.name}</Tag>
+              )}
+            </React.Fragment>
+          ))}
+        </TagList>
+      )}
+    </FilterBox>
+  );
+}
+
+export default FilterTag;
+
+const FilterBox = styled.div`
+  width: 100;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+const TagList = styled.div`
+  width: 100;
+  display: flex;
+  justify-content: center;
+  height: 60px;
+  overflow-y: scroll;
+  height: 90px;
+`;
+const Tag = styled.div`
+  display: inline;
+  margin: 10px;
+  background-color: red;
+  height: 30px;
+  width: 50px;
+  border-radius: 5px;
+`;
+
+const SelectedTagList = styled.div`
+  display: flex;
+  margin: 10px;
+  width: 100;
+`;
+
+const SelectedTag = styled.div`
+  border: 1px solid;
+  border-radius: 5px;
+`;
--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -2,32 +2,33 @@ import styled from "styled-components";
 import Input from "components/common/Input";
 import Category from "components/common/Category";
 import { ImageNameButton } from "components/common/ImageNameButton";
-import { ImagePreviewButton } from "components/common/ImagePreviewButton";
+import FilterTag from "components/common/FilterTag";
 
 function CategoryPage() {
   return (
     <>
       <Category />
-
+      <FilterTag />
       <InputBox>
-        <Input placeholder="상품명을 입력해 주세요." />
+        <label>상품명</label>
+        <Input width="600" placeholder="상품명을 입력해 주세요." />
         <span>{"{ 상품코드 }"}</span>
       </InputBox>
       <InputBox>
-        <Input placeholder="상품 구성 소개 정보를 입력해 주세요." />
+        <label>상품 구성 소개 정보</label>
+        <Input width="600" placeholder="상품 구성 소개 정보를 입력해 주세요." />
       </InputBox>
       <ImageBox>
         <span>상품 썸네일</span>
-        <ImageNameButton isMultiple={false} />
+        <ImageNameButton isMultiple={false} inputId="thumbnailImg" />
       </ImageBox>
       <ImageBox>
         <span>상품 대표 이미지</span>
-        <ImageNameButton isMultiple={true} />
-        {/* <ImagePreviewButton /> */}
+        <ImageNameButton isMultiple={true} inputId="representImg" />
       </ImageBox>
       <TextBox>
         <span>상품 총 재고</span>
-        <span>{"{ NN 개 }"}</span>
+        <span> 41 개</span>
       </TextBox>
     </>
   );
@@ -37,10 +38,14 @@ export default CategoryPage;
 
 const InputBox = styled.div`
   margin: 10px;
+  width: 100;
 `;
 const ImageBox = styled.div`
   margin: 10px;
+  width: 100;
+  display: flex;
 `;
 const TextBox = styled.div`
   margin: 10px;
+  width: 100;
 `;
