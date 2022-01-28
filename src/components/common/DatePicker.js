b1e2ad86746c50b76435d91c07a1067118ece75f
kykim00 <jeongsaeki@likelion.org>
2022-01-28 19:05:44 +0900

    feat: 상품 기한 설정 페이지 구현 refactor: disabled props로 받아오기 style: 커서 깜빡임 수정 fix: label 클릭 되도록 수정
    feat: 다른 종류 datepicker 추가
    
    refactor: 기존 datepicker 컴포넌트 이름 변경
    
    refactor: 기존 datepicker 컴포넌트 이름 변경

--- src/components/common/DatePicker.js
+++ src/components/common/DatePicker.js
@@ -6,36 +6,15 @@ import theme from 'styles/theme';
 
 export const DatePickerTemplate = () => {
   const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
   return (
-    <DatePickerContainer>
-      <DatePickerInput
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-        selectsStart
-        startDate={startDate}
-        endDate={endDate}
-        showTimeSelect
-        timeIntervals={15}
-        timeCaption="Time"
-        dateFormat="yyyy.MM.dd hh:mm aa"
-        placeholderText="YYYY.MM.DD YY:MM"
-      />
-      <span>~</span>
-      <DatePickerInput
-        selected={endDate}
-        onChange={(date) => setEndDate(date)}
-        selectsEnd
-        startDate={startDate}
-        endDate={endDate}
-        minDate={startDate}
-        showTimeSelect
-        timeIntervals={15}
-        timeCaption="Time"
-        dateFormat="yyyy.MM.dd hh:mm aa"
-        placeholderText="YYYY.MM.DD YY:MM"
-      />
-    </DatePickerContainer>
+    <DatePickerInput
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+      selectsStart
+      startDate={startDate}
+      dateFormat="yyyy.MM.dd"
+      placeholderText="YYYY.MM.DD"
+    />
   );
 };
 
@@ -44,9 +23,6 @@ const DatePickerInput = styled(DatePicker)`
   border-radius: 5px;
   border: 1.5px solid ${theme.colors.lightPurple};
   margin-left: 5px;
-`;
-
-const DatePickerContainer = styled.div`
-  display: flex;
-  align-items: center;
+  color: transparent;
+  text-shadow: 0 0 0 black;
 `;
