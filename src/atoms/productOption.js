27d6f62c6cbc8b752c09d9095b20c4757b4b9fe2
이도현 <ksmfou98@naver.com>
2022-01-29 14:00:07 +0900

    fix: 주석 제거

--- src/atoms/productOption.js
+++ src/atoms/productOption.js
@@ -1,28 +1,4 @@
-import { atom, selectorFamily, useRecoilValue } from "recoil";
-
-// const ProductOptionMocks = [
-//   {
-//     id: 1,
-//     img: "",
-//     options: [
-//       {
-//         id: 1,
-//         name: "",
-//         normalPrice: 0,
-//         salePrice: 0,
-//         stock: 0,
-//         tax: "",
-//         additionalOptions: [
-//           {
-//             id: 1,
-//             additionalName: "",
-//             additionalNormalPrice: 0,
-//           },
-//         ],
-//       },
-//     ],
-//   },
-// ];
+import { atom } from "recoil";
 
 export const productOptionState = atom({
   key: "productOptionState",
